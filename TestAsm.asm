15737d117a85c1629f07d498ad8e3e427255c196
nambles <crzyhunter@gmail.com>
2016-11-15 15:37:56 -0500

    Add files via upload

--- TestAsm.asm
+++ TestAsm.asm
@@ -1,13 +1,15 @@
-; SimpleRobotProgram.asm
-; Created by Kevin Johnson
-; (no copyright applied; edit freely, no attribution necessary)
-; This program does basic initialization of the DE2Bot
-; and provides an example of some robot control.
+; Fall2016StartingPoint.asm
+; This program includes a basic movement API that allows the
+; user to specify a desired heading and speed, and the API will
+; attempt to control the robot in an appropriate way.
 
-; Section labels are for clarity only.
-
-ORG 0  ; Begin program at x000
-
+; This code uses the timer interrupt for the control code.
+ORG 0                  ; Jump table is located in mem 0-4
+	JUMP   Init        ; Reset vector
+	RETI               ; Sonar interrupt (unused)
+	JUMP   CTimer_ISR  ; Timer interrupt
+	RETI               ; UART interrupt (unused)
+	RETI               ; Motor stall interrupt (unused)
 
 ;***************************************************************
 ;* Initialization
@@ -57,75 +59,81 @@ WaitForUser:
 ;***************************************************************
 ;* Main code
 ;***************************************************************
-Main: ; "Real" program starts here.
+Main:
 	OUT    RESETPOS    ; reset odometer in case wheels moved after programming
+	
+	; configure timer interrupts
+	LOADI  10          ; 10ms * 10 = 0.1s rate, or 10Hz.
+	OUT    CTIMER      ; turn on timer peripheral
+	SEI    &B0010      ; enable interrupts from source 2 (timer)
+	; at this point, timer interrupts will be firing at 10Hz, and
+	; code in that ISR will attempt to control the robot.
+	; If you want to take manual control of the robot,
+	; execute a CLI &B0010 to disable the interrupt.
+	
 
-;/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-; All of this code ("EnableSonars" through "SimpleTask") is purely for example.
-; It starts the robot moving forward and stops it when either something is
-; detected in front of the robot or it has traveled four feet.
-EnableSonars:
-	; Many convenient constants are defined at the bottom of this file.
-	LOAD   Mask4
-	;OR     Mask3       ; AC now contains 0b01100
-	OUT    SONAREN     ; enable front-facing sensors
 	
-SimpleTask:
-	; check for objects in front of robot
-	;IN     DIST2       ; get value from sensor 2
-	;ADDI   -610        ; about 2ft
-	; Because the new SCOMP has an 11-bit operand, we can use
-	; immediate values up to +/-1023
-	;JNEG   Die         ; routine to immediately stop the robot
-	
-	; repeat for sensor 3
-	;IN     DIST3
-	;ADDI   -610        ; about 2ft
-	;JNEG   Die         ; routine to immediately stop the robot
+	LOAD	Mask0
+	OR		Mask1
+	OR		Mask2
+	OR		Mask3
+	OR		Mask4
+	OR		Mask5	
+	OUT		SONAREN	; Enable al 6 forward-facing sensors
 	
 	
-	IN	   DIST4	;Sensor 5 Jump to TURN if detects something within 2 feet
-	SUB    104
-	JNEG   TOOCLOSE
-	IN	   DIST4
-	SUB	   208
-	JPOS   TOOFAR 
+;WallFollow:
+;	IN		Dist5	; Take in the distance to the closest object on the right
+;	SUB		Dist0	; Take in the distance to the closest object on the left
+;	JPOS	LeftWall ; If the distance to the left object is greater than the distance to the right object, we have the wall to the bot's left
 	
+RightWallSetup:			; Instructions to follow along the right wall
+	IN		DIST4
+	STORE	D4
+	ADDI	-40
+	STORE	D4Low
 	
-	; check distance traveled.
-	; robot should be moving in +X direction.
-	;IN     XPOS
-	;SUB    TwoFeet     ; just using because it was already defined
-	;SUB    TwoFeet
-	;JPOS   Die
-	
-	; If no case above stops the robot, then at this point
-	; the robot should continue moving forward.
-	LOAD   FMid        ; defined below
-	OUT    LVELCMD     ; send velocity to left and right wheels
-	OUT    RVELCMD
-	JUMP   SimpleTask
-	
-; End example code
-;/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-
-TOOCLOSE:
-	LOAD	FFast
+RightWall:
+	LOAD	FMid
 	OUT		RVELCMD
-	JUMP	SimpleTask
-	
-TOOFAR:
-	LOAD	FFast
 	OUT		LVELCMD
-	JUMP	SimpleTask
+	IN		DIST4
+	SUB		D4Low
+	JNEG	RClose
+	ADDI	-80
+	JPOS	RFar
+	JUMP	RightWall
+	
+		
+RClose:
+	LOAD	FSlow
+	OUT		LVELCMD
+	LOAD	FMid
+	OUT		RVELCMD
+	IN		DIST4
+	SUB		D4
+	JPOS	RightWall
+	JUMP	RClose
+	
+RFar:
+	LOAD	FSlow
+	OUT		RVELCMD
+	LOAD	FMid
+	OUT		LVELCMD
+	IN		DIST4
+	SUB		D4
+	JNEG	RightWall
+	JUMP	RFar
 	
 	
+
 
 	
 Die:
 ; Sometimes it's useful to permanently stop execution.
 ; This will also catch the execution if it accidentally
 ; falls through from above.
+	CLI    &B1111       ; disable all interrupts
 	LOAD   Zero         ; Stop everything.
 	OUT    LVELCMD
 	OUT    RVELCMD
@@ -136,10 +144,430 @@ Forever:
 	JUMP   Forever      ; Do this forever.
 	DEAD:  DW &HDEAD    ; Example of a "local" variable
 
+
+; Timer ISR.  Currently just calls the control code
+CTimer_ISR:
+	CALL   ControlMovement
+	RETI   ; return from ISR
 	
+	
+; Control code.  If called repeatedly, this code will attempt
+; to control the robot to face the angle specified in DTheta
+; and match the speed specified in DVel
+DTheta:    DW 0
+DVel:      DW 0
+ControlMovement:
+	; convenient way to get +/-180 angle error is
+	; ((error + 180) % 360 ) - 180
+	IN     THETA
+	SUB    DTheta      ; actual - desired angle
+	CALL   Neg         ; desired - actual angle
+	ADDI   180
+	CALL   Mod360
+	ADDI   -180
+	; A quick-and-dirty way to get a decent velocity value
+	; for turning is to multiply the angular error by 4.
+	SHIFT  2
+	STORE  CMAErr      ; hold temporarily
+
+	
+	; For this basic control method, simply take the
+	; desired forward velocity and add a differential
+	; velocity for each wheel when turning is needed.
+	LOAD   DVel
+	ADD    CMAErr
+	CALL   CapVel      ; ensure velocity is valid
+	OUT    RVELCMD
+	LOAD   CMAErr
+	CALL   Neg         ; left wheel gets negative differential
+	ADD    DVel
+	CALL   CapVel
+	OUT    LVELCMD
+	
+	RETURN
+	CMAErr: DW 0       ; holds angle error velocity
+
+CapVel:
+	; cap velocity values for the motors
+	ADDI    -500
+	JPOS    CapVelHigh
+	ADDI    500
+	ADDI    500
+	JNEG    CapVelLow
+	ADDI    -500
+	RETURN
+CapVelHigh:
+	LOADI   500
+	RETURN
+CapVelLow:
+	LOADI   -500
+	RETURN
+
 ;***************************************************************
 ;* Subroutines
 ;***************************************************************
+
+
+;*******************************************************************************
+; Mod360: modulo 360
+; Returns AC%360 in AC
+; Written by Kevin Johnson.  No licence or copyright applied.
+;*******************************************************************************
+Mod360:
+	; easy modulo: subtract 360 until negative then add 360 until not negative
+	JNEG   M360N
+	ADDI   -360
+	JUMP   Mod360
+M360N:
+	ADDI   360
+	JNEG   M360N
+	RETURN
+
+;*******************************************************************************
+; Abs: 2's complement absolute value
+; Returns abs(AC) in AC
+; Neg: 2's complement negation
+; Returns -AC in AC
+; Written by Kevin Johnson.  No licence or copyright applied.
+;*******************************************************************************
+Abs:
+	JPOS   Abs_r
+Neg:
+	XOR    NegOne       ; Flip all bits
+	ADDI   1            ; Add one (i.e. negate number)
+Abs_r:
+	RETURN
+
+;******************************************************************************;
+; Atan2: 4-quadrant arctangent calculation                                     ;
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
+; Original code by Team AKKA, Spring 2015.                                     ;
+; Based on methods by Richard Lyons                                            ;
+; Code updated by Kevin Johnson to use software mult and div                   ;
+; No license or copyright applied.                                             ;
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
+; To use: store dX and dY in global variables AtanX and AtanY.                 ;
+; Call Atan2                                                                   ;
+; Result (angle [0,359]) is returned in AC                                     ;
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
+; Requires additional subroutines:                                             ;
+; - Mult16s: 16x16->32bit signed multiplication                                ;
+; - Div16s: 16/16->16R16 signed division                                       ;
+; - Abs: Absolute value                                                        ;
+; Requires additional constants:                                               ;
+; - One:     DW 1                                                              ;
+; - NegOne:  DW 0                                                              ;
+; - LowByte: DW &HFF                                                           ;
+;******************************************************************************;
+Atan2:
+	LOAD   AtanY
+	CALL   Abs          ; abs(y)
+	STORE  AtanT
+	LOAD   AtanX        ; abs(x)
+	CALL   Abs
+	SUB    AtanT        ; abs(x) - abs(y)
+	JNEG   A2_sw        ; if abs(y) > abs(x), switch arguments.
+	LOAD   AtanX        ; Octants 1, 4, 5, 8
+	JNEG   A2_R3
+	CALL   A2_calc      ; Octants 1, 8
+	JNEG   A2_R1n
+	RETURN              ; Return raw value if in octant 1
+A2_R1n: ; region 1 negative
+	ADDI   360          ; Add 360 if we are in octant 8
+	RETURN
+A2_R3: ; region 3
+	CALL   A2_calc      ; Octants 4, 5            
+	ADDI   180          ; theta' = theta + 180
+	RETURN
+A2_sw: ; switch arguments; octants 2, 3, 6, 7 
+	LOAD   AtanY        ; Swap input arguments
+	STORE  AtanT
+	LOAD   AtanX
+	STORE  AtanY
+	LOAD   AtanT
+	STORE  AtanX
+	JPOS   A2_R2        ; If Y positive, octants 2,3
+	CALL   A2_calc      ; else octants 6, 7
+	CALL   Neg          ; Negatge the number
+	ADDI   270          ; theta' = 270 - theta
+	RETURN
+A2_R2: ; region 2
+	CALL   A2_calc      ; Octants 2, 3
+	CALL   Neg          ; negate the angle
+	ADDI   90           ; theta' = 90 - theta
+	RETURN
+A2_calc:
+	; calculates R/(1 + 0.28125*R^2)
+	LOAD   AtanY
+	STORE  d16sN        ; Y in numerator
+	LOAD   AtanX
+	STORE  d16sD        ; X in denominator
+	CALL   A2_div       ; divide
+	LOAD   dres16sQ     ; get the quotient (remainder ignored)
+	STORE  AtanRatio
+	STORE  m16sA
+	STORE  m16sB
+	CALL   A2_mult      ; X^2
+	STORE  m16sA
+	LOAD   A2c
+	STORE  m16sB
+	CALL   A2_mult
+	ADDI   256          ; 256/256+0.28125X^2
+	STORE  d16sD
+	LOAD   AtanRatio
+	STORE  d16sN        ; Ratio in numerator
+	CALL   A2_div       ; divide
+	LOAD   dres16sQ     ; get the quotient (remainder ignored)
+	STORE  m16sA        ; <= result in radians
+	LOAD   A2cd         ; degree conversion factor
+	STORE  m16sB
+	CALL   A2_mult      ; convert to degrees
+	STORE  AtanT
+	SHIFT  -7           ; check 7th bit
+	AND    One
+	JZERO  A2_rdwn      ; round down
+	LOAD   AtanT
+	SHIFT  -8
+	ADDI   1            ; round up
+	RETURN
+A2_rdwn:
+	LOAD   AtanT
+	SHIFT  -8           ; round down
+	RETURN
+A2_mult: ; multiply, and return bits 23..8 of result
+	CALL   Mult16s
+	LOAD   mres16sH
+	SHIFT  8            ; move high word of result up 8 bits
+	STORE  mres16sH
+	LOAD   mres16sL
+	SHIFT  -8           ; move low word of result down 8 bits
+	AND    LowByte
+	OR     mres16sH     ; combine high and low words of result
+	RETURN
+A2_div: ; 16-bit division scaled by 256, minimizing error
+	LOADI  9            ; loop 8 times (256 = 2^8)
+	STORE  AtanT
+A2_DL:
+	LOAD   AtanT
+	ADDI   -1
+	JPOS   A2_DN        ; not done; continue shifting
+	CALL   Div16s       ; do the standard division
+	RETURN
+A2_DN:
+	STORE  AtanT
+	LOAD   d16sN        ; start by trying to scale the numerator
+	SHIFT  1
+	XOR    d16sN        ; if the sign changed,
+	JNEG   A2_DD        ; switch to scaling the denominator
+	XOR    d16sN        ; get back shifted version
+	STORE  d16sN
+	JUMP   A2_DL
+A2_DD:
+	LOAD   d16sD
+	SHIFT  -1           ; have to scale denominator
+	STORE  d16sD
+	JUMP   A2_DL
+AtanX:      DW 0
+AtanY:      DW 0
+AtanRatio:  DW 0        ; =y/x
+AtanT:      DW 0        ; temporary value
+A2c:        DW 72       ; 72/256=0.28125, with 8 fractional bits
+A2cd:       DW 14668    ; = 180/pi with 8 fractional bits
+
+;*******************************************************************************
+; Mult16s:  16x16 -> 32-bit signed multiplication
+; Based on Booth's algorithm.
+; Written by Kevin Johnson.  No licence or copyright applied.
+; Warning: does not work with factor B = -32768 (most-negative number).
+; To use:
+; - Store factors in m16sA and m16sB.
+; - Call Mult16s
+; - Result is stored in mres16sH and mres16sL (high and low words).
+;*******************************************************************************
+Mult16s:
+	LOADI  0
+	STORE  m16sc        ; clear carry
+	STORE  mres16sH     ; clear result
+	LOADI  16           ; load 16 to counter
+Mult16s_loop:
+	STORE  mcnt16s      
+	LOAD   m16sc        ; check the carry (from previous iteration)
+	JZERO  Mult16s_noc  ; if no carry, move on
+	LOAD   mres16sH     ; if a carry, 
+	ADD    m16sA        ;  add multiplicand to result H
+	STORE  mres16sH
+Mult16s_noc: ; no carry
+	LOAD   m16sB
+	AND    One          ; check bit 0 of multiplier
+	STORE  m16sc        ; save as next carry
+	JZERO  Mult16s_sh   ; if no carry, move on to shift
+	LOAD   mres16sH     ; if bit 0 set,
+	SUB    m16sA        ;  subtract multiplicand from result H
+	STORE  mres16sH
+Mult16s_sh:
+	LOAD   m16sB
+	SHIFT  -1           ; shift result L >>1
+	AND    c7FFF        ; clear msb
+	STORE  m16sB
+	LOAD   mres16sH     ; load result H
+	SHIFT  15           ; move lsb to msb
+	OR     m16sB
+	STORE  m16sB        ; result L now includes carry out from H
+	LOAD   mres16sH
+	SHIFT  -1
+	STORE  mres16sH     ; shift result H >>1
+	LOAD   mcnt16s
+	ADDI   -1           ; check counter
+	JPOS   Mult16s_loop ; need to iterate 16 times
+	LOAD   m16sB
+	STORE  mres16sL     ; multiplier and result L shared a word
+	RETURN              ; Done
+c7FFF: DW &H7FFF
+m16sA: DW 0 ; multiplicand
+m16sB: DW 0 ; multipler
+m16sc: DW 0 ; carry
+mcnt16s: DW 0 ; counter
+mres16sL: DW 0 ; result low
+mres16sH: DW 0 ; result high
+
+;*******************************************************************************
+; Div16s:  16/16 -> 16 R16 signed division
+; Written by Kevin Johnson.  No licence or copyright applied.
+; Warning: results undefined if denominator = 0.
+; To use:
+; - Store numerator in d16sN and denominator in d16sD.
+; - Call Div16s
+; - Result is stored in dres16sQ and dres16sR (quotient and remainder).
+; Requires Abs subroutine
+;*******************************************************************************
+Div16s:
+	LOADI  0
+	STORE  dres16sR     ; clear remainder result
+	STORE  d16sC1       ; clear carry
+	LOAD   d16sN
+	XOR    d16sD
+	STORE  d16sS        ; sign determination = N XOR D
+	LOADI  17
+	STORE  d16sT        ; preload counter with 17 (16+1)
+	LOAD   d16sD
+	CALL   Abs          ; take absolute value of denominator
+	STORE  d16sD
+	LOAD   d16sN
+	CALL   Abs          ; take absolute value of numerator
+	STORE  d16sN
+Div16s_loop:
+	LOAD   d16sN
+	SHIFT  -15          ; get msb
+	AND    One          ; only msb (because shift is arithmetic)
+	STORE  d16sC2       ; store as carry
+	LOAD   d16sN
+	SHIFT  1            ; shift <<1
+	OR     d16sC1       ; with carry
+	STORE  d16sN
+	LOAD   d16sT
+	ADDI   -1           ; decrement counter
+	JZERO  Div16s_sign  ; if finished looping, finalize result
+	STORE  d16sT
+	LOAD   dres16sR
+	SHIFT  1            ; shift remainder
+	OR     d16sC2       ; with carry from other shift
+	SUB    d16sD        ; subtract denominator from remainder
+	JNEG   Div16s_add   ; if negative, need to add it back
+	STORE  dres16sR
+	LOADI  1
+	STORE  d16sC1       ; set carry
+	JUMP   Div16s_loop
+Div16s_add:
+	ADD    d16sD        ; add denominator back in
+	STORE  dres16sR
+	LOADI  0
+	STORE  d16sC1       ; clear carry
+	JUMP   Div16s_loop
+Div16s_sign:
+	LOAD   d16sN
+	STORE  dres16sQ     ; numerator was used to hold quotient result
+	LOAD   d16sS        ; check the sign indicator
+	JNEG   Div16s_neg
+	RETURN
+Div16s_neg:
+	LOAD   dres16sQ     ; need to negate the result
+	CALL   Neg
+	STORE  dres16sQ
+	RETURN	
+d16sN: DW 0 ; numerator
+d16sD: DW 0 ; denominator
+d16sS: DW 0 ; sign value
+d16sT: DW 0 ; temp counter
+d16sC1: DW 0 ; carry value
+d16sC2: DW 0 ; carry value
+dres16sQ: DW 0 ; quotient result
+dres16sR: DW 0 ; remainder result
+
+;*******************************************************************************
+; L2Estimate:  Pythagorean distance estimation
+; Written by Kevin Johnson.  No licence or copyright applied.
+; Warning: this is *not* an exact function.  I think it's most wrong
+; on the axes, and maybe at 45 degrees.
+; To use:
+; - Store X and Y offset in L2X and L2Y.
+; - Call L2Estimate
+; - Result is returned in AC.
+; Result will be in same units as inputs.
+; Requires Abs and Mult16s subroutines.
+;*******************************************************************************
+L2Estimate:
+	; take abs() of each value, and find the largest one
+	LOAD   L2X
+	CALL   Abs
+	STORE  L2T1
+	LOAD   L2Y
+	CALL   Abs
+	SUB    L2T1
+	JNEG   GDSwap    ; swap if needed to get largest value in X
+	ADD    L2T1
+CalcDist:
+	; Calculation is max(X,Y)*0.961+min(X,Y)*0.406
+	STORE  m16sa
+	LOADI  246       ; max * 246
+	STORE  m16sB
+	CALL   Mult16s
+	LOAD   mres16sH
+	SHIFT  8
+	STORE  L2T2
+	LOAD   mres16sL
+	SHIFT  -8        ; / 256
+	AND    LowByte
+	OR     L2T2
+	STORE  L2T3
+	LOAD   L2T1
+	STORE  m16sa
+	LOADI  104       ; min * 104
+	STORE  m16sB
+	CALL   Mult16s
+	LOAD   mres16sH
+	SHIFT  8
+	STORE  L2T2
+	LOAD   mres16sL
+	SHIFT  -8        ; / 256
+	AND    LowByte
+	OR     L2T2
+	ADD    L2T3     ; sum
+	RETURN
+GDSwap: ; swaps the incoming X and Y
+	ADD    L2T1
+	STORE  L2T2
+	LOAD   L2T1
+	STORE  L2T3
+	LOAD   L2T2
+	STORE  L2T1
+	LOAD   L2T3
+	JUMP   CalcDist
+L2X:  DW 0
+L2Y:  DW 0
+L2T1: DW 0
+L2T2: DW 0
+L2T3: DW 0
+
 
 ; Subroutine to wait (block) for 1 second
 Wait1:
@@ -164,7 +592,7 @@ BattCheck:
 ; If the battery is too low, we want to make
 ; sure that the user realizes it...
 DeadBatt:
-	LOAD   Four
+	LOADI  &H20
 	OUT    BEEP        ; start beep sound
 	CALL   GetBattLvl  ; get the battery level
 	OUT    SSEG1       ; display it everywhere
@@ -228,6 +656,8 @@ I2CError:
 ;* Variables
 ;***************************************************************
 Temp:     DW 0 ; "Temp" is not a great name, but can be useful
+D4:		 DW	0 ;
+D4Low:   DW 0 ; Stored value of Sensor 0
 
 ;***************************************************************
 ;* Constants
